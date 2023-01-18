1b399c42b8ce50d4ce31ae5c1521393f91da0610
4mbk0r <4mbk0r@example.com>
2023-01-18 15:16:43 -0400

    afdsf

--- resources/js/Pages/Micomponet/Excel.vue
+++ resources/js/Pages/Micomponet/Excel.vue
@@ -1,98 +1,65 @@
+<!-- Use preprocessors via the lang attribute! e.g. <template lang="pug"> -->
 <template>
     <v-app>
-        <section>
-            <h3>Create XLSX</h3>
+        <div id="app">
+            <h1>excel import with vue & sheet.js!</h1>
             <div>
-                <input v-model="sheetName" placeholder="type a new sheet name" />
-                <button v-if="sheetName" @click="addSheet">Add Sheet</button>
+                <input type="file" id="excelFile" @change="excelExport"
+                    accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" />
             </div>
-
-            <div>Sheets: {{ sheets }}</div>
-
-            <xlsx-workbook>
-                <xlsx-sheet :collection="sheet.data" v-for="sheet in sheets" :key="sheet.name"
-                    :sheet-name="sheet.name" />
-                <xlsx-download>
-                    <button>Download</button>
-                </xlsx-download>
-            </xlsx-workbook>
-        </section>
-        <hr />
-        <section>
-            <h3>Import XLSX</h3>
-            <input type="file" @change="onChange" />
-            <xlsx-read :file="file">
-                <xlsx-sheets>
-                    <template #default="{ sheets }">
-                        <select v-model="selectedSheet">
-                            <option v-for="sheet in sheets" :key="sheet" :value="sheet">
-                                {{ sheet }}
-                            </option>
-                        </select>
-                    </template>
-                </xlsx-sheets>
-                <!--<xlsx-table :sheet="selectedSheet" />-->
-                <xlsx-json :sheet="selectedSheet">
-                    <template #default="{ collection }">
-
-
-                        {{ mostrar(collection)}}
-
-                    </template>
-                </xlsx-json>
-            </xlsx-read>
-        </section>
-        <div class="wrapper-dgxl">
-            <div ref="dgxl" class="grid"></div>
-            <input type="button" value="Add new row" @click="dgxlObj.insertEmptyRows()" />
-            <input type="button" value="Download data as CSV" @click="dgxlObj.downloadDataAsCSV()" /><br />
+            <div>
+                {{ mostrar(this.excelData) }}
+            </div>
+            <div class="wrapper-dgxl">
+                <div ref="dgxl" class="grid"></div>
+                <input type="button" value="Add new row" @click="dgxlObj.insertEmptyRows()" />
+                <input type="button" value="Download data as CSV" @click="dgxlObj.downloadDataAsCSV()" /><br />
+            </div>
         </div>
     </v-app>
 </template>
 
 <script>
-import { XlsxRead, XlsxTable, XlsxSheets, XlsxJson, XlsxWorkbook, XlsxSheet, XlsxDownload } from "vue-xlsx"
-import DataGridXL from "@datagridxl/datagridxl2";
 
+import XLSX from 'xlsx'
+import DataGridXL from "@datagridxl/datagridxl2";
 export default {
-    name: "DataGrid",
-    components: {
-        XlsxRead,
-        XlsxTable,
-        XlsxSheets,
-        XlsxJson,
-        XlsxWorkbook,
-        XlsxSheet,
-        XlsxDownload
-    },
     data() {
         return {
+            excelData: [{ a: 1, b: 3 }],
             file: null,
             selectedSheet: null,
             sheetName: null,
             sheets: [{ name: "SheetOne", data: [{ c: 2 }] }],
             collection: [{ a: 1, b: 3 }],
-
         };
     },
-    computed: {
-        dgxlOptions() {
-            return {
-                data: this.collection
-            };
-        },
-    },
-    mounted: function () {
-        const dgxlObj = new DataGridXL(this.$refs.dgxl, this.dgxlOptions);
-        Object.assign(this, { dgxlObj }); // tucks all methods under dgxlObj object in component instance
-    },
     methods: {
+        excelExport(event) {
+            var input = event.target;
+            var reader = new FileReader();
+            reader.onload = () => {
+                var fileData = reader.result;
+                var wb = XLSX.read(fileData, { type: 'binary' });
+                wb.SheetNames.forEach((sheetName) => {
+                    var rowObj = XLSX.utils.sheet_to_json(wb.Sheets[sheetName], { defval: "" });
+                    this.excelData = JSON.stringify(rowObj)
+                    console.log(JSON.parse(this.excelData))
+                    const dgxlObj = new DataGridXL(this.$refs.dgxl, {
+                        data: JSON.parse(this.excelData)
+                    });
+                    
+                })
+            };
+
+            reader.readAsBinaryString(input.files[0]);
+
+
+        },
         mostrar(x) {
-            console.log(x)
-            const dgxlObj = new DataGridXL(this.$refs.dgxl, {
-                data: x
-            });
-            x = []
+            //console.log(x)
+            //const dgxlObj = new DataGridXL(this.$refs.dgxl, {data: x});
+            //x = []
             //Object.assign(this, { dgxlObj })
 
         },
@@ -112,10 +79,28 @@ export default {
 
             this.sheetName = null;
         }
-    }
+    },
+    computed: {
+
+    },
+    mounted: function () {
+        const dgxlObj = new DataGridXL(this.$refs.dgxl,{
+                        data: this.excelData
+                    });
+        Object.assign(this, { dgxlObj }); // tucks all methods under dgxlObj object in component instance
+    },
 };
 </script>
+
+<!-- Use preprocessors via the lang attribute! e.g. <style lang="scss"> -->
 <style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+
 .grid {
     height: 400px;
 }
--- resources/js/Pages/Micomponet/excel2.vue
+++ resources/js/Pages/Micomponet/excel2.vue
@@ -1,112 +1,122 @@
 <template>
-    <app-layout>
-        <v-parallax>
+    <v-app>
+        <section>
+            <h3>Create XLSX</h3>
+            <div>
+                <input v-model="sheetName" placeholder="type a new sheet name" />
+                <button v-if="sheetName" @click="addSheet">Add Sheet</button>
+            </div>
 
-        </v-parallax>
-    </app-layout>
+            <div>Sheets: {{ sheets }}</div>
+
+            <xlsx-workbook>
+                <xlsx-sheet :collection="sheet.data" v-for="sheet in sheets" :key="sheet.name"
+                    :sheet-name="sheet.name" />
+                <xlsx-download>
+                    <button>Download</button>
+                </xlsx-download>
+            </xlsx-workbook>
+        </section>
+        <hr />
+        <section>
+            <h3>Import XLSX</h3>
+            <input type="file" @change="onChange" />
+            <xlsx-read :file="file">
+                <xlsx-sheets>
+                    <template #default="{ sheets }">
+                        <select v-model="selectedSheet">
+                            <option v-for="sheet in sheets" :key="sheet" :value="sheet">
+                                {{ sheet }}
+                            </option>
+                        </select>
+                    </template>
+                </xlsx-sheets>
+                <!--<xlsx-table :sheet="selectedSheet" />-->
+                <xlsx-json :sheet="selectedSheet">
+                    <template #default="{ collection }">
+
+
+                        {{ mostrar(collection)}}
+
+                    </template>
+                </xlsx-json>
+            </xlsx-read>
+        </section>
+        <div class="wrapper-dgxl">
+            <div ref="dgxl" class="grid"></div>
+            <input type="button" value="Add new row" @click="dgxlObj.insertEmptyRows()" />
+            <input type="button" value="Download data as CSV" @click="dgxlObj.downloadDataAsCSV()" /><br />
+        </div>
+    </v-app>
 </template>
 
-<style scoped>
-.bg-gray-100 {
-    background-color: #f7fafc;
-    background-color: rgba(247, 250, 252, var(--tw-bg-opacity));
-}
-
-.border-gray-200 {
-    border-color: #edf2f7;
-    border-color: rgba(237, 242, 247, var(--tw-border-opacity));
-}
-
-.text-gray-400 {
-    color: #cbd5e0;
-    color: rgba(203, 213, 224, var(--tw-text-opacity));
-}
-
-.text-gray-500 {
-    color: #a0aec0;
-    color: rgba(160, 174, 192, var(--tw-text-opacity));
-}
-
-.text-gray-600 {
-    color: #718096;
-    color: rgba(113, 128, 150, var(--tw-text-opacity));
-}
-
-.text-gray-700 {
-    color: #4a5568;
-    color: rgba(74, 85, 104, var(--tw-text-opacity));
-}
-
-.text-gray-900 {
-    color: #1a202c;
-    color: rgba(26, 32, 44, var(--tw-text-opacity));
-}
-
-@media (prefers-color-scheme: dark) {
-    .dark\:bg-gray-800 {
-        background-color: #2d3748;
-        background-color: rgba(45, 55, 72, var(--tw-bg-opacity));
-    }
-
-    .dark\:bg-gray-900 {
-        background-color: #1a202c;
-        background-color: rgba(26, 32, 44, var(--tw-bg-opacity));
-    }
-
-    .dark\:border-gray-700 {
-        border-color: #4a5568;
-        border-color: rgba(74, 85, 104, var(--tw-border-opacity));
-    }
-
-    .dark\:text-white {
-        color: #fff;
-        color: rgba(255, 255, 255, var(--tw-text-opacity));
-    }
-
-    .dark\:text-gray-400 {
-        color: #cbd5e0;
-        color: rgba(203, 213, 224, var(--tw-text-opacity));
-    }
-}
-</style>
-
 <script>
-import JetApplicationMark from '@/Jetstream/ApplicationMark'
-import JetBanner from '@/Jetstream/Banner'
-import JetDropdown from '@/Jetstream/Dropdown'
-import JetDropdownLink from '@/Jetstream/DropdownLink'
-import JetNavLink from '@/Jetstream/NavLink'
-import JetResponsiveNavLink from '@/Jetstream/ResponsiveNavLink'
-import AppLayout from '@/Layouts/AppLayout'
-import { Inertia } from '@inertiajs/inertia'
+import { XlsxRead, XlsxTable, XlsxSheets, XlsxJson, XlsxWorkbook, XlsxSheet, XlsxDownload } from "vue-xlsx"
+import DataGridXL from "@datagridxl/datagridxl2";
 
 export default {
-
-    data: () => ({
-        drawer: null
-    }),
-    props: {
-        canLogin: Boolean,
-        canRegister: Boolean,
-        laravelVersion: String,
-        phpVersion: String,
-    }, components: {
-        JetApplicationMark,
-        JetBanner,
-        JetDropdown,
-        JetDropdownLink,
-        JetNavLink,
-        JetResponsiveNavLink,
-        AppLayout,
+    name: "DataGrid",
+    components: {
+        XlsxRead,
+        XlsxTable,
+        XlsxSheets,
+        XlsxJson,
+        XlsxWorkbook,
+        XlsxSheet,
+        XlsxDownload
     },
-    created() {
-        console.log("fasfads")
-        console.log(this.$page.props.user)
-        if (this.$page.props.user == null) {
-            Inertia.post(route('logout'))
+    data() {
+        return {
+            file: null,
+            selectedSheet: null,
+            sheetName: null,
+            sheets: [{ name: "SheetOne", data: [{ c: 2 }] }],
+            collection: [{ a: 1, b: 3 }],
+
+        };
+    },
+    computed: {
+        dgxlOptions() {
+            return {
+                data: this.collection
+            };
+        },
+    },
+    mounted: function () {
+        const dgxlObj = new DataGridXL(this.$refs.dgxl, this.dgxlOptions);
+        Object.assign(this, { dgxlObj }); // tucks all methods under dgxlObj object in component instance
+    },
+    methods: {
+        mostrar(x) {
+            console.log(x)
+            const dgxlObj = new DataGridXL(this.$refs.dgxl, {
+                data: x
+            });
+            x = []
+            //Object.assign(this, { dgxlObj })
+
+        },
+        onChange(event) {
+
+            this.file = event.target.files ? event.target.files[0] : null;
+            console.log("-----")
+            console.log(event)
+            const dgxlObj = new DataGridXL(this.$refs.dgxl, {
+                data: event
+            });
+        },
+        addSheet() {
+
+
+            this.sheets.push({ name: this.sheetName, data: [...this.collection] });
+
+            this.sheetName = null;
         }
-
-
     }
-}
+};
 </script>
+<style>
+.grid {
+    height: 400px;
+}
+</style>
