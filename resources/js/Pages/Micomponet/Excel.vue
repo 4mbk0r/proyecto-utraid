c1687b784eac69bdc0951e9f269e7c9747c925b0
4mbk0r <4mbk0r@example.com>
2023-01-19 14:43:07 -0400

    excewwl

--- resources/js/Pages/Micomponet/Excel.vue
+++ resources/js/Pages/Micomponet/Excel.vue
@@ -2,7 +2,7 @@
 <template>
     <v-app>
         <div id="app">
-            <h1>excel import with vue & sheet.js!</h1>
+            <h1>Importar</h1>
             <div>
                 <input type="file" id="excelFile" @change="excelExport"
                     accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" />
@@ -41,6 +41,8 @@ export default {
             reader.onload = () => {
                 var fileData = reader.result;
                 var wb = XLSX.read(fileData, { type: 'binary' });
+                const sheetList = wb.SheetNames; //Array of sheet names.
+                console.log(sheetList)
                 wb.SheetNames.forEach((sheetName) => {
                     var rowObj = XLSX.utils.sheet_to_json(wb.Sheets[sheetName], { defval: "" });
                     this.excelData = JSON.stringify(rowObj)
@@ -48,7 +50,7 @@ export default {
                     const dgxlObj = new DataGridXL(this.$refs.dgxl, {
                         data: JSON.parse(this.excelData)
                     });
-                    
+
                 })
             };
 
@@ -69,7 +71,8 @@ export default {
             console.log("-----")
             console.log(event)
             const dgxlObj = new DataGridXL(this.$refs.dgxl, {
-                data: event
+                data: event,
+                locale: 'es'
             });
         },
         addSheet() {
@@ -84,9 +87,27 @@ export default {
 
     },
     mounted: function () {
-        const dgxlObj = new DataGridXL(this.$refs.dgxl,{
-                        data: this.excelData
-                    });
+        var dgxl_nl_NL = {
+
+            // Context Menu Item Presets
+
+            "Copy": "Copiar",
+            "Cut": "Cortar",
+            "Paste": "Pegar",
+            "Delete Row(s)": "Eliminar Filas(s)",
+            "Insert Row(s) (up)": "Insertar Fila(s) Encima",
+            "Insert Row(s) (down)": "Insertar Fila(s) Debajo",
+            "Delete Column(s)": "Eliminar Columna(s)",
+            "Insert Column(s) (left)": "Insertar Columna(s) Derecha",
+            "Insert Column(s) (right)": "Insertar Columna(s) Izquierda",
+            "Deselect": "Deseleccionar",
+            "Search": "Buscar",
+
+        };
+        const dgxlObj = new DataGridXL(this.$refs.dgxl, {
+            data: this.excelData,
+            locale: dgxl_nl_NL
+        });
         Object.assign(this, { dgxlObj }); // tucks all methods under dgxlObj object in component instance
     },
 };
