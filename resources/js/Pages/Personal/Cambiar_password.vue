bd1e8bc4b13de1c137ab5c0e51e9c0c45e235e6b
4mbk0r <4mbk0r@gmail.com>
2023-03-29 08:24:32 -0400

    dsaf

---
+++ resources/js/Pages/Personal/Cambiar_password.vue
@@ -0,0 +1,104 @@
+<template>
+    <v-dialog v-model="dialog">
+        <v-card>
+            <v-card-title>
+                Cambiar Contraseña
+            </v-card-title>
+            <v-card-text>
+                <v-form>
+                    <v-row>
+                        <v-col>
+                            <v-text-field dense outlined label="Nuevo Password" type="text" v-model="dato.password"
+                                :rules="[v => !!v || 'Se requiere Password']" required class="mb-n4 pa-0"
+                                prepend-inner-icon="mdi-account-key" />
+                        </v-col>
+
+                    </v-row>
+                    <v-row>
+                        <v-col>
+                            <v-text-field dense outlined label="Confirmar Password" type="text" v-model="dato.conf_password"
+                                :rules="[v => !!v || 'Se requiere Confirmar Password']" required class="mb-n4 pa-0"
+                                prepend-inner-icon="mdi-account-key" />
+                        </v-col>
+
+                    </v-row>
+                    <v-row>
+                        <v-col>
+                            <v-btn color="primary" variant="text" @click="cambiar_password()">
+                                Cambiar contraseña
+                            </v-btn>
+                        </v-col>
+
+                    </v-row>
+
+                </v-form>
+                <v-row>
+                    <v-col>
+                        <v-btn color="success" variant="text" @click="cambiar_password_default()">
+                            Contraseña Predefinidas
+                        </v-btn>
+
+                    </v-col>
+
+                </v-row>
+
+                <v-spacer>
+
+                </v-spacer>
+
+
+
+            </v-card-text>
+            <v-card-actions>
+
+                <v-btn color="primary" variant="text" @click="close()">
+                    Close
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+import { emit } from 'process';
+
+
+
+export default {
+    props: {
+    },
+    data() {
+        return {
+            dato: {},
+            dialog: true,
+        }
+    },
+    mounted() {
+
+    },
+    components: {
+
+    },
+    created() {
+        //console.log(cargos)
+    },
+    computed: {
+        update(validate) {
+
+        }
+
+    },
+    watch: {
+
+    },
+    methods: {
+        close() {
+            this.$emit('close', false)
+        }
+    }
+
+}
+</script>
+    
+
+
--- resources/js/Pages/Personal/Cuenta.vue
+++ resources/js/Pages/Personal/Cuenta.vue
@@ -10,34 +10,31 @@
                 </v-card-title>
 
                 <v-row style="background-color: #00a5a0">
-                    
+
                     <v-col cols="12" style="background-color: #a2315a">
 
-                        <v-img contain max-height="40" max-width="100%" class="ml-auto"
-                            src="./assets/logo-sedes-lapaz.png">
+                        <v-img contain max-height="40" max-width="100%" class="ml-auto" src="./assets/logo-sedes-lapaz.png">
                         </v-img>
                     </v-col>
                 </v-row>
                 <v-row>
                     <v-col>
-                        <v-text-field dense outlined id="nombre" label="Nombres" type="text" v-model="$page.props.user.nombre"
-                            :rules="[v => !!v || ' Se requiere completar Nombre']" required autofocus
-                            prepend-inner-icon="mdi-account-arrow-right-outline" class="mb-n5 pa-0"
-                            @input="(val) => ( form.nombre = val.toUpperCase())"
-                            pattern="[a-zA-Z]+" />
+                        <v-text-field dense outlined id="nombre" label="Nombres" type="text"
+                            v-model="$page.props.user.nombre" :rules="[v => !!v || ' Se requiere completar Nombre']"
+                            required autofocus prepend-inner-icon="mdi-account-arrow-right-outline" class="mb-n5 pa-0"
+                            @input="(val) => (form.nombre = val.toUpperCase())" pattern="[a-zA-Z]+" />
                     </v-col>
                     <v-col>
                         <v-text-field dense outlined id="paterno" label="Apellido Paterno" type="text"
-                            v-model="$page.props.user.ap_paterno" :rules="[v => !!v || 'Se requiere Completar Apellido Paterno']"
+                            v-model="$page.props.user.ap_paterno"
+                            :rules="[v => !!v || 'Se requiere Completar Apellido Paterno']"
                             prepend-inner-icon="mdi-account-arrow-right-outline" required class="mb-n5 pa-0"
-                            @input="(val) => ( form.ap_paterno = val.toUpperCase())"
-                            pattern="[a-zA-Z]+" />
+                            @input="(val) => (form.ap_paterno = val.toUpperCase())" pattern="[a-zA-Z]+" />
                     </v-col>
                     <v-col>
                         <v-text-field dense outlined id="materno" label="Apellido Materno" type="text"
                             v-model="form.ap_materno" :rules="[v => !!v || 'Se requiere Completar Apellido Materno']"
-                            @input="(val) => ( form.ap_materno = val.toUpperCase())"
-                            
+                            @input="(val) => (form.ap_materno = val.toUpperCase())"
                             prepend-inner-icon="mdi-account-arrow-right-outline" required class="mb-n5 pa-0"
                             pattern="[a-zA-Z]+" />
                     </v-col>
@@ -45,8 +42,8 @@
                 <v-row>
                     <v-col>
                         <v-text-field dense outlined id="CI" label="Cedula de Identidad" type="number" v-model="form.ci"
-                            :rules="[v => !!v || 'Se requiere completar Cedula de Identidad']" required
-                            class="mb-n4 pa-0" prepend-inner-icon="mdi-card-account-details-outline" />
+                            :rules="[v => !!v || 'Se requiere completar Cedula de Identidad']" required class="mb-n4 pa-0"
+                            prepend-inner-icon="mdi-card-account-details-outline" />
                     </v-col>
                     <v-col>
                         <v-text-field dense outlined id="item" label="Item" type="text" v-model="form.item"
@@ -64,10 +61,8 @@
                         required prepend-inner-icon="mdi-clipboard-account">
                     </v-text-field>
                     -->
-                    <v-text-field dense outlined  
-                        v-model="form.cargo"
-                        :rules="[v => !!v || 'Se requiere el Cargo']" filled label="Asigne un cargo" class="mb-n4 pa-0"
-                        required prepend-inner-icon="mdi-clipboard-account">
+                    <v-text-field dense outlined v-model="form.cargo" :rules="[v => !!v || 'Se requiere el Cargo']" filled
+                        label="Asigne un cargo" class="mb-n4 pa-0" required prepend-inner-icon="mdi-clipboard-account">
                     </v-text-field>
                 </v-row>
                 <v-row>
@@ -100,38 +95,38 @@
                 </div>
                 -->
                 <v-row>
-                <v-col >
-                    <!--<inertia-link :href="route('login')"
+                    <v-col>
+                        <!--<inertia-link :href="route('login')"
                             class="underline text-sm text-gray-600 hover:text-gray-900">
                             Already registered?
                         </inertia-link>
                         -->
-                    <v-btn class="ml-4" color="primary" type="sumbit" :class="{ 'opacity-25': form.processing }"
-                        :disabled="form.processing">
-                        Guardar Datos de perfil
-                    </v-btn>
-                </v-col>
-                <v-col >
-                    <!--<inertia-link :href="route('login')"
+                        <v-btn class="ml-4" color="primary" type="sumbit" :class="{ 'opacity-25': form.processing }"
+                            :disabled="form.processing">
+                            Guardar Datos de perfil
+                        </v-btn>
+                    </v-col>
+                    <v-col>
+                        <!--<inertia-link :href="route('login')"
                             class="underline text-sm text-gray-600 hover:text-gray-900">
                             Already registered?
                         </inertia-link>
                         -->
-                    <v-btn class="ml-4" color="primary" type="sumbit" :class="{ 'opacity-25': form.processing }"
-                        :disabled="form.processing" 
-                        @click="ff("
-                        
-                        >
-                        Cambiar Contraseña
-                    </v-btn>
-                </v-col>
-            </v-row>
+                        <v-btn class="ml-4" color="primary" :class="{ 'opacity-25': form.processing }"
+                            :disabled="form.processing" @click="change_password">
+                            Cambiar Contraseña
+                        </v-btn>
+                    </v-col>
+                </v-row>
             </v-form>
         </v-card>
+        <cambiar_password v-if="dialog_cambio" @close="close_dialog($event)"></cambiar_password>
     </app-layout>
 </template>
 
 <script>
+
+import cambio from '@/Pages/Personal/Cambiar_password'
 import AppLayout from '@/Layouts/AppLayout'
 import JetAuthenticationCard from '@/Jetstream/AuthenticationCard'
 import JetAuthenticationCardLogo from '@/Jetstream/AuthenticationCardLogo'
@@ -140,14 +135,16 @@ import JetInput from '@/Jetstream/Input'
 import JetCheckbox from "@/Jetstream/Checkbox";
 import JetLabel from '@/Jetstream/Label'
 import JetValidationErrors from '@/Jetstream/ValidationErrors'
+import Cambiar_password from './Cambiar_password.vue'
 
 
 export default {
-    props:{
-       
+    props: {
+
     },
     data() {
         return {
+            dialog_cambio: false,
             form: this.$inertia.form({
                 nombre: '',
                 ap_paterno: '',
@@ -167,9 +164,9 @@ export default {
             //['Admin', 'Medico General', 'Trabajo Social', 'Operador Terapético', 'Psicologo', 'Psicologo', 'Secretaria', 'recepcionista'],
         }
     },
-    mounted(){
-        this.form =  this.$page.props.user
-    }, 
+    mounted() {
+        this.form = this.$page.props.user
+    },
     components: {
         AppLayout,
         JetAuthenticationCard,
@@ -179,21 +176,47 @@ export default {
         JetCheckbox,
         JetLabel,
         JetValidationErrors,
+        Cambiar_password
 
     },
     created() {
         //console.log(cargos)
     },
-    computed:{
-        update(validate){
+    destroyed() {
+        //this.dialog_cambio = false
+    },
+    computed: {
+        update(validate) {
 
         }
 
-    },  
-    watch:{
+    },
+    watch: {
 
-    },  
+    },
     methods: {
+        close_dialog($e) {
+            this.dialog_cambio = false
+        },
+        async change_password() {
+            this.dialog_cambio = true
+            /*
+            var res = await axios({
+                method: 'post',
+                url: `/${process.env.MIX_CARPETA}/change_password`,
+                data: this.form
+            }).then(
+                (response) => {
+                    console.log(response);
+                }
+            ).catch(err => {
+                console.log(err)
+                console.log("err->", err.response.data)
+                return res.status(500).send({ ret_code: ReturnCodes.SOMETHING_WENT_WRONG });
+            });
+            this.$alert('hoas').then(res => this.$inform("Cambiar contraseña!"));
+            */
+        },
         alert(text) {
             this.$alert(text).then(res => this.$inform("Cambios guardados!"));
         },
